13db1750122a990e55da1463c99fbeb9e8c345ca
Binay <143321061+binay-das@users.noreply.github.com>
2026-08-22 01:47:59 +0530

    add cors

--- apps/chess-backend/src/index.ts
+++ apps/chess-backend/src/index.ts
@@ -1,9 +1,15 @@
+import "dotenv/config";
 import express from "express";
+import cors from "cors";
+import authRouter from "./routes/auth.routes.js";
 
 const app = express();
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT!;
 
+app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", authRouter);
 
 app.get("/health", (_req, res) => {
   res.json({ status: "ok" });
--- apps/chess-backend/src/routes/auth.routes.ts
+++ apps/chess-backend/src/routes/auth.routes.ts
@@ -73,6 +73,8 @@ router.post("/signup", async (req: Request, res: Response) => {
 
 
     } catch (error) {
+        console.log(error);
+
         return res.status(500).json({
             success: false,
             message: "Internal server error",
@@ -139,6 +141,8 @@ router.post("/signin", async (req: Request, res: Response) => {
             }
         })
     } catch (error) {
+        console.log(error);
+
         return res.status(500).json({
             success: false,
             message: "Internal server error",
